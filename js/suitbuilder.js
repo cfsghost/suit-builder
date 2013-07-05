4eb83dccc95dbb874ef18a70db9190705f4f0dde
Fred Chien <cfsghost@gmail.com>
2013-07-05 16:38:22 +0800

    Fixed some bugs and add tab component

--- js/suitbuilder.js
+++ js/suitbuilder.js
@@ -1,5 +1,6 @@
 $(function() {
 
+	// Navbar
 	$('#widget_navbar').draggable({
 		appendTo: 'body',
 		connectToSortable: '#viewer',
@@ -27,30 +28,70 @@ $(function() {
 
 			return $e;
 		},
-		start: function(event, ui) {
-//			ui.item = ui.helper;
+		revert: 'invalid'
+	});
+
+	// Tab
+	$('#widget_navtab').draggable({
+		appendTo: 'body',
+		connectToSortable: '#viewer',
+		helper: function(event) {
+			var $e = $('<div>');
+
+			// Tab
+			var $nav = $('<ul>').addClass('nav nav-tabs');
+			var $item1 = $('<li>');
+			var $label1 = $('<a>').attr({
+				'href': '#pane1',
+				'data-toggle': 'tab'
+			}).text('Tab1');
+
+			var $item2 = $('<li>');
+			var $label2 = $('<a>').attr({
+				'href': '#pane2',
+				'data-toggle': 'tab'
+			}).text('Tab2');
+
+			$nav.append($item1);
+			$nav.append($item2);
+
+			$item1.append($label1);
+			$item2.append($label2);
+
+			// Content
+			var $content = $('<div>').addClass('tab-content');
+			var $pane1 = $('<div>').addClass('tab-pane').attr('id', 'pane1');
+			var $pane2 = $('<div>').addClass('tab-pane').attr('id', 'pane2');
+
+			$content.append($pane1).append($pane2);
+
+			$e.append($nav).append($content);
+
+			return $e;
 		},
 		revert: 'invalid'
 	});
 
+	var externalObj = null;
 	$('#viewer')
-	/*
-		.droppable({
-			drop: function(event, ui) {
-				var $item = $(ui.helper).clone(false);
-
-				$item
-					.removeClass('ui-draggable-dragging')
-				   .css({ position: 'relative', left: 0, top: 0 });  
-
-				$(this).append($item);
-			}
-		})
-		*/
 		.sortable({
 			placeholder: 'ui-sortable-placeholder',
 			update: function(event, ui) {
-				$(ui.item).remove();
+				console.log('update');
+				if (externalObj) {
+
+					externalObj.insertAfter(ui.item);
+
+					$(ui.item).remove();
+				}
+
+				externalObj = null;
+			},
+			start: function(event, ui) {
+				console.log('start');
+			},
+			stop: function(event, ui) {
+				console.log('stop');
 			},
 			sort: function(event, ui) {
 				console.log('sort');
@@ -65,9 +106,9 @@ $(function() {
 				$item.css({ visibility: 'hidden' });
 
 				//$ph.replaceWith($item);
-				$ph.html($item.clone(false).wrap('<p>').parent().html());
+				$ph.html($item.clone(false).wrap('<div>').parent().html());
 				$ph.css({
-					height: $item.outerHeight()
+					height: $(ui.helper).outerHeight() * 2
 				});
 				console.log(ui.placeholder);
 			},
@@ -75,10 +116,10 @@ $(function() {
 				var $item = $(ui.helper).clone(true);
 
 				$item
-					.removeClass('ui-draggable-dragging')
-				   .css({ position: 'relative', left: 0, top: 0 });  
+					.removeClass('ui-draggable-dragging ui-sortable-helper')
+					.css({ position: 'relative', left: 0, top: 0, height: '' });  
 
-				$(this).append($item);
+				externalObj = $item;
 			}
 		})
 		.disableSelection();
